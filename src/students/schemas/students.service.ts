d3f3df70d45ef213fca1aa034ab9726d3cf7b254
Khushal <khushal.work247@gmail.com>
2026-01-14 08:44:48 +0530

    Create students.service.ts

---
+++ src/students/schemas/students.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { StudentsRepository } from './students.repository';
+import { CreateStudentDto } from './dto/create-student.dto';
+
+@Injectable()
+export class StudentsService {
+  constructor(private studentsRepository: StudentsRepository) {}
+
+  async create(createStudentDto: CreateStudentDto) {
+    return this.studentsRepository.create(createStudentDto);
+  }
+
+  async findAll() {
+    return this.studentsRepository.findAll();
+  }
+
+  async findByUserId(userId: string) {
+    return this.studentsRepository.findByUserId(userId);
+  }
+
+  async enrollInCourse(studentId: string, courseId: string) {
+    return this.studentsRepository.enrollInCourse(studentId, courseId);
+  }
+}
