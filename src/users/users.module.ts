d09fbcdfc9ba8692df90f41d4941bb316ab32c20
Khushal <khushal.work247@gmail.com>
2026-01-14 08:31:51 +0530

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './user.schema';
+import { UsersRepository } from './users.repository';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-imports: [
-MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
-],
-controllers: [UsersController],
-providers: [UsersService],
-exports: [UsersService]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService, UsersRepository], // Export so other modules can use
 })
 export class UsersModule {}
