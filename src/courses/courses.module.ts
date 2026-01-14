f90b49ca46bed969a9d6b000b9a1633dd03ecb15
Khushal <khushal.work247@gmail.com>
2026-01-14 08:55:31 +0530

    Update courses.module.ts

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CourseSchema } from './courses.schema';
+import { CoursesRepository } from './courses.repository';
+import { Course, CourseSchema } from './schemas/course.schema';
 
 @Module({
-imports:[
-MongooseModule.forFeature([{name:'Course',schema:CourseSchema}])
-],
-controllers:[CoursesController],
-providers:[CoursesService],
-exports: [CoursesService]
+  imports: [
+    MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]),
+  ],
+  controllers: [CoursesController],
+  providers: [CoursesService, CoursesRepository],
+  exports: [CoursesService],
 })
 export class CoursesModule {}
