812434dc8669d3ebc0b84273ca2dce4e2d2ab4e7
Khushal <khushal.work247@gmail.com>
2026-01-14 08:50:39 +0530

    Create professors.repository.ts

---
+++ src/professors/professors.repository.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Professor } from './schemas/professor.schema';
+import { CreateProfessorDto } from './dto/create-professor.dto';
+
+@Injectable()
+export class ProfessorsRepository {
+  constructor(
+    @InjectModel(Professor.name) private professorModel: Model<Professor>,
+  ) {}
+
+  async create(createProfessorDto: CreateProfessorDto): Promise<Professor> {
+    const professor = new this.professorModel(createProfessorDto);
+    return professor.save();
+  }
+
+  async findByUserId(userId: string): Promise<Professor | null> {
+    return this.professorModel
+      .findOne({ userId })
+      .populate('teachingCourses')
+      .exec();
+  }
+
+  async findAll(): Promise<Professor[]> {
+    return this.professorModel.find().populate('userId', 'name email').exec();
+  }
+
+  async assignCourse(professorId: string, courseId: string): Promise<Professor> {
+    return this.professorModel
+      .findByIdAndUpdate(
+        professorId,
+        { $addToSet: { teachingCourses: courseId } },
+        { new: true },
+      )
+      .exec();
+  }
+}
